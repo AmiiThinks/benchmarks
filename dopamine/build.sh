93a1da05f9575354ab26b59acaa8d9f6713d6330
andy <andnpatterson@gmail.com>
2023-06-07 21:34:53 -0600

    add main rainbow/atari benchmark

--- dopamine/build.sh
+++ dopamine/build.sh
@@ -1,4 +1,8 @@
-mkdir roms/
-wget http://www.atarimania.com/roms/Roms.rar roms/
+if [ ! -f ./Roms.rar ]; then
+    wget http://www.atarimania.com/roms/Roms.rar -O ./Roms.rar
+fi
 
-docker build
+docker build -f core/Dockerfile -t dopamine/core .
+docker build -f atari/Dockerfile -t dopamine/atari .
+
+time docker run --privileged --gpus all --name rainbow_atari -it dopamine/atari  python -um dopamine.discrete_domains.train --base_dir /tmp --gin_files /configs/rainbow.gin
