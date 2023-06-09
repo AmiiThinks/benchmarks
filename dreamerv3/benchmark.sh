bd88e1b080d20903d6c5f42e73abe679080bef96
Jacob Adkins <jacob.adkins18@ncf.edu>
2023-06-09 15:28:29 -0600

    reduce steps

--- dreamerv3/benchmark.sh
+++ dreamerv3/benchmark.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Beginning atari benchmark."
-{ time python3 train.py --logdir ./logs --configs atari --run.steps 15000 ; }  &>> ./dreamer_output.log
+{ time python3 train.py --logdir ./logs --configs atari --run.steps 5000 ; }  &>> ./dreamer_output.log
 rm -rf ./logs
 echo "Beginning dmc benchmark."
-{ time python3 train.py --logdir ./logs --configs dmc_vision --run.steps 10000 ; }  &>> ./dreamer_output.log
+{ time python3 train.py --logdir ./logs --configs dmc_vision --run.steps 5000 ; }  &>> ./dreamer_output.log
 rm -rf ./logs
 echo "Beginning crafter benchmark."
-{ time python3 train.py --logdir ./logs --configs crafter --run.steps 4000 ; } &>> ./dreamer_output.log
+{ time python3 train.py --logdir ./logs --configs crafter --run.steps 2000 ; } &>> ./dreamer_output.log
 
