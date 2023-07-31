02e9912456844c0016734d2ee19383f6577bca2e
Karim Ali <kali2@ualberta.ca>
2023-07-31 11:40:58 -0600

    Update recurrent_ppo to work with CUDA 12 and fix flax version change

--- recurrent_ppo/run.sh
+++ recurrent_ppo/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run  -it recurrent_ppo python3 /root/recurrent_ppo/src/control_exps.py --config-name=config_ppo_lstm_popgym
-docker run  -it recurrent_ppo python3 /root/recurrent_ppo/src/control_exps.py --config-name=config_ppo_gru_popgym
+docker run --gpus all -it recurrent_ppo python3 /root/recurrent_ppo/src/control_exps.py --config-name=config_ppo_lstm_popgym
+docker run --gpus all -it recurrent_ppo python3 /root/recurrent_ppo/src/control_exps.py --config-name=config_ppo_gru_popgym
